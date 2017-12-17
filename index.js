28d6eda5f3f08d7281db3cbfb2147aa33b059d04
Van A. Dao <daoanhvan81@gmail.com>
2017-12-17 23:42:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObj = Object.assign({}, obj);
+  var newObj = Object.assign({}, object);
   newObj[key] = value;
   return newObj;
 }
