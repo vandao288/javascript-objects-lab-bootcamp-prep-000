9dbc9224f38dfc1d07cc5f02d193f4fd12ec7207
Van A. Dao <daoanhvan81@gmail.com>
2017-12-17 23:36:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObj = Object.assign({}, obj);
+  newObj[key] = value;
+  return newObj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}
+
